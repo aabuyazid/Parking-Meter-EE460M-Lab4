07a3263dbb292fa973f9b30702ee6a92ce065a14
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2019-03-05 18:36:34 -0600

    Hi

--- Decrementer.v
+++ Decrementer.v
@@ -1,18 +1,28 @@
 `timescale 1ns / 1ps
 
 module Decrementer(
-    input second,
+    input half_second,
     input [15:0] adder_value,
-    output [15:0] decrement_value
+    output [15:0] decrement_value,
+    output below200
 );
 
+reg decrement = 0;
 reg [15:0] decrement_value_reg;
 assign decrement_value = decrement_value_reg;
+assign below200 = (decrement_value < 200) ? 1:0;
+always @(*) begin
+    if(decrement) begin
+        if(adder_value > 0) 
+            decrement_value_reg = adder_value - 1;
+        else
+            decrement_value_reg = 0;
+    end
+    else 
+        decrement_value_reg = adder_value;
+end
 
-always @(posedge second) begin
-    if(adder_value > 0) 
-        decrement_value_reg = adder_value - 1;
-    else
-        decrement_value_reg = 0;
+always @(posedge half_second) begin
+    decrement = ~decrement;
 end
 endmodule
