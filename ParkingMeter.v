1e910067702a42a1f0d4f4803fc507822d3c5fd1
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2019-03-05 18:41:13 -0600

    modified:   ParkingMeter.v

--- ParkingMeter.v
+++ ParkingMeter.v
@@ -4,11 +4,14 @@ module ParkingMeter(
     input clk,
     input u, l, r, d,
     input sw0, sw1,
+    output dp,
     output [3:0] an,
     output [6:0] sseg
 );
 
 wire SSEGclk, second, half_second;
+
+wire dp;
 
 wire below200; // if high, should start blinking
 
@@ -16,14 +19,17 @@ wire [15:0] adder_value, decrement_value, digits;
 
 wire [27:0] sev_seg_data;
 
+assign dp = 1;
+
 // All clocks used 
 Second sec (.clk(clk),.second(second));
 HalfSecond half (.clk(clk),.half_second(half_second));
 SSEGClkDivider Sclk (.clk(clk),.slow_clk(SSEGclk));
 
-// Missing Incrementer
-    
-Decrementer dec (.second(second),.half_second(half_second),
+Incrementer inc (.clk(clk), .u(u), .l(l), .r(r), .d(d), 
+    .sw0(sw0), .sw1(sw1), .timeIn(decrement_value), .timeOut(adder_value));
+
+Decrementer dec (.half_second(half_second),
     .adder_value(adder_value),.decrement_value(decrement_value),
     .below200(below200));
 
